426158924cbe861ab4d14c6459539621a962941e
K-Dardis <K.J.J.A.Dardis@outlook.com>
2023-04-22 18:27:25 +0100

    added a Title and Logo to Window

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ pygame.init()
 #Create Window
 WIN = pygame.display.set_mode((800, 800))
 
+#Set Title and Icon
+win_title = "Checkers"
+win_icon = pygame.image.load("Resources/Images/Checkers-Logo.png")
+
+pygame.display.set_caption(win_title)
+pygame.display.set_icon(win_icon)
+
 #Create Game loop
 running = True
 while running:
